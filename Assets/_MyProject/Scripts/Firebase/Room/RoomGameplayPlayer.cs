9028c6f00519880f5f19c9310f16f78195c3bcaa
Filip <filpkrcmarevic555@gmail.com>
2024-12-22 19:28:32 +0100

    Save point

--- Assets/_MyProject/Scripts/Firebase/Room/RoomGameplayPlayer.cs
+++ Assets/_MyProject/Scripts/Firebase/Room/RoomGameplayPlayer.cs
@@ -1,28 +1,15 @@
 using System;
 using Newtonsoft.Json;
-using UnityEngine;
 
 [Serializable]
 public class RoomGameplayPlayer
 {
-    private int actionsLeft;
     public string PlayerId;
     public int LootChange;
     public int StrangeMatter;
     public int AmountOfAbilitiesPlayerCanBuy;
 
-    public int ActionsLeft
-    {
-        get
-        {
-            return actionsLeft;
-        }
-        set
-        {
-            Debug.Log("Setting actions to: "+value+ " for:"+IsMy);
-            actionsLeft = value;
-        }
-    }
+    public int ActionsLeft;
 
     [JsonIgnore] public bool IsMy => PlayerId == FirebaseManager.Instance.PlayerId;
 }
