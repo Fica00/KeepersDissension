f29e65cd84eea5165c05b10b1ef6158452eae02d
Filip <filpkrcmarevic555@gmail.com>
2024-12-27 16:51:51 +0100

    .

--- Assets/_MyProject/Scripts/Gameplay/Cards/FactionEffects/Base/ScoutVision.cs
+++ Assets/_MyProject/Scripts/Gameplay/Cards/FactionEffects/Base/ScoutVision.cs
@@ -1,4 +1,33 @@
+using System.Collections.Generic;
+
 public class ScoutVision : CardSpecialAbility
 {
-    
+    private void OnEnable()
+    {
+        GameplayManager.OnCardMoved += CheckCardThatMoved;
+    }
+
+    private void OnDisable()
+    {
+        GameplayManager.OnCardMoved -= CheckCardThatMoved;
+    }
+
+    private void CheckCardThatMoved(CardBase _cardThatMoved, int _startingPlace, int _finishingPlace)
+    {
+        if (_cardThatMoved != CardBase)
+        {
+            return;
+        }
+
+        List<Card> _cardsAround = GameplayManager.Instance.TableHandler.GetAttackableCards(_finishingPlace, CardMovementType.EightDirections);
+        foreach (var _cardAround in _cardsAround)
+        {
+            if (_cardAround is not Marker _marker)
+            {
+                continue;
+            }
+            
+            GameplayManager.Instance.DamageCardByAbility(_marker.UniqueId, 1,null);
+        }
+    }
 }
