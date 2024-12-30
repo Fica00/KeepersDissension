787ba9557a6fb2d1b415a51250bdf5ac7df347c2
Filip <filpkrcmarevic555@gmail.com>
2024-12-30 23:20:09 +0100

    .

--- Assets/_MyProject/Scripts/Gameplay/UI/ActivationFieldHandler.cs
+++ Assets/_MyProject/Scripts/Gameplay/UI/ActivationFieldHandler.cs
@@ -7,19 +7,14 @@ public class ActivationFieldHandler : MonoBehaviour
 {
     public static Action OnShowed;
     public static Action OnHided;
+    
     [SerializeField] private GameObject holder;
     [SerializeField] private Button closeButton;
     [SerializeField] private Transform cardsHolder;
     
-    private Vector3 sizeOfCards = new (2, 2, 1);
-    private List<CardBase> shownCards = new ();
-    private int placeId;
-
-
-    
     private void OnEnable()
     {
-        TablePlaceHandler.OnPlaceClicked -= CheckPlace;
+        TablePlaceHandler.OnPlaceClicked += CheckPlace;
         CardTableInteractions.OnPlaceClicked += CheckPlace;
         closeButton.onClick.AddListener(Close);
     }
@@ -38,7 +33,7 @@ public class ActivationFieldHandler : MonoBehaviour
             return;
         }
 
-        placeId = _clickedPlace.Id;
+        Debug.Log("Should show cards");
         ShowCards(_clickedPlace.GetCards());
     }
 
@@ -49,116 +44,14 @@ public class ActivationFieldHandler : MonoBehaviour
             return;
         }
         
-        foreach (var _card in _cardBase)
-        {
-            _card.transform.SetParent(cardsHolder);
-            _card.PositionInHand();
-            _card.transform.localScale = sizeOfCards;
-            _card.gameObject.AddComponent<CardHandInteractions>().Setup(_card);
-            shownCards.Add(_card);
-        }
-        
         holder.SetActive(true);
-        CardHandInteractions.OnCardClicked += CheckCard;
         OnShowed?.Invoke();
     }
 
     private void Close()
     {
-        ClearShownCards();
         holder.SetActive(false);
-        CardHandInteractions.OnCardClicked -= CheckCard;
         OnHided?.Invoke();
     }
 
-    private void ClearShownCards()
-    {
-        TablePlaceHandler _activationField = GameplayManager.Instance.TableHandler.GetPlace(placeId);
-        foreach (var _shownCard in shownCards)
-        {
-            CardHandInteractions _cardHandler = _shownCard.GetComponent<CardHandInteractions>();
-            if (_cardHandler!=null)
-            {
-                Destroy(_cardHandler);
-            }
-            _shownCard.PositionOnTable(_activationField);
-        }
-        
-        shownCards.Clear();
-    }
-    
-    private void CheckCard(CardBase _cardBase)
-    {
-        if (GameplayManager.Instance.IsResponseAction())
-        {
-            if (!GameplayManager.Instance.IsMyResponseAction())
-            {
-                return;
-            }
-
-            if (!GameplayManager.Instance.IsKeeperResponseAction)
-            {
-                return;
-            }
-        }
-        
-        if (!GameplayManager.Instance.IsMyTurn())
-        {
-            return;
-        }
-        
-        if (!shownCards.Contains(_cardBase))
-        {
-            return;
-        }
-        
-        if (GameplayManager.Instance.IsAbilityActive<Subdued>() && GameplayCheats.CheckForCd)
-        {
-            DialogsManager.Instance.ShowOkDialog("Activation of the ability is blocked by Subdued ability");
-            return;
-        }
-
-        int _indexOfCard = 1;
-        foreach (var _shownCard in shownCards)
-        {
-            if (_shownCard==_cardBase)
-            {
-                break;
-            }
-
-            _indexOfCard++;
-        }
-
-        AbilityEffect _effect = (_cardBase as AbilityCard)?.Effect;
-        int _amountOfCardsOnTop = shownCards.Count - _indexOfCard;
-        bool _canReturn = _effect.Cooldown <= _amountOfCardsOnTop;
-        if (!GameplayCheats.CheckForCd)
-        {
-            _canReturn = true;
-        }
-        if (_canReturn)
-        {
-            CardTableInteractions _tableInteractions = _cardBase.GetComponent<CardTableInteractions>();
-            if (_tableInteractions!=null)
-            {
-                Destroy(_tableInteractions);
-            }
-
-            CardHandInteractions _handInteractions = _cardBase.GetComponent<CardHandInteractions>();
-            if (_handInteractions!=null)
-            {
-                Destroy(_handInteractions);
-            }
-            shownCards.Remove(_cardBase);
-            _cardBase.transform.SetParent(null);
-            _cardBase.PositionInHand();
-            Close();
-            GameplayManager.Instance.ReturnAbilityFromActivationField(((AbilityCard)_cardBase).UniqueId);
-        }
-        else
-        {
-            DialogsManager.Instance.ShowOkDialog($"This card needs {_effect.Cooldown} on top of it but there is only " +
-                                            $"{_amountOfCardsOnTop}");
-        }
-    }
 }
