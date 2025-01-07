d78d44ecca6551219f744d8905c6a75335104a4a
Filip <filpkrcmarevic555@gmail.com>
2025-01-07 22:10:46 +0100

    .

--- Assets/_MyProject/Scripts/Gameplay/GameplayManager/GameplayManager.cs
+++ Assets/_MyProject/Scripts/Gameplay/GameplayManager/GameplayManager.cs
@@ -111,7 +111,10 @@ public class GameplayManager : MonoBehaviour
 
         yield return new WaitForSeconds(1f);
         AbilityCardsManagerBase.Instance.Setup();
-        RoomUpdater.Instance.ForceUpdate();
+        if (FirebaseManager.Instance.RoomHandler.IsOwner)
+        {
+            RoomUpdater.Instance.ForceUpdate();
+        }
         yield return new WaitForSeconds(1f);
 
         FinishedSetup?.Invoke();
